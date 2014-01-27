712679b6072e37c5d5bb8b2f2312f3ba9fb49611
Nico Mandery <nico@nmandery.net>
2014-01-27 20:50:51 +0100

    schema checks for geojson structures

--- src/geotoolkit/geojson/schema.clj
+++ src/geotoolkit/geojson/schema.clj
@@ -3,22 +3,48 @@
     [schema.core :as s] ))
 
 
-(def CoordinateList s/Any) ; TODO
+(def Coordinate 
+  (s/both
+    [s/Num]
+    (s/pred #(> (count %) 1) "has at least 2 coordinate numbers")))
+
+(def CoordinateListLevel1 [Coordinate])
+(def CoordinateListLevel2 [CoordinateListLevel1])
+
+(def CoordinateList 
+  (s/either CoordinateListLevel1 CoordinateListLevel2))
 
 (def Geometry {
-  :type s/Str
-  :coordinates CoordinateList      
+  :type (s/both
+    s/Str
+    (s/pred 
+      (fn [x] (some #(= x %) 
+        ["Point" "Polygon" "LineString"]))
+      "has non-multi geometry type"))
+  :coordinates (s/either Coordinate CoordinateList)
 })
 
 (def MultiGeometry {
-  :type s/Str
-  :geometries s/Any ; TODO
+  :type (s/both
+    s/Str
+    (s/pred 
+      (fn [x] (some #(= x %) 
+        ["MultiPoint" "MultiPolygon" "MultiLineString" "GeometryCollection"]))
+      "has multigeometry type"))
+  :geometries [Geometry]
 })
 
 (def Feature { 
   :geometry (s/either Geometry MultiGeometry)
   :properties {
-    s/Keyword s/Any
+    (s/either s/Str s/Keyword) s/Any
   }
-  :type s/Str
+  :type (s/both s/Str 
+    (s/pred #(= % "Feature") "type of feature is 'Feature'"))
+})
+
+(def FeatureCollection {
+  :features [Feature]
+  :type (s/both s/Str 
+    (s/pred #(= % "FeatureCollection") "type of featurecollection is 'FeatureCollection'"))
 })
