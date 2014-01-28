3aabf2f6399ef8411b28f2c814df8fb075a45f88
Nico Mandery <nico.mandery@geops.de>
2014-01-28 17:28:38 +0100

    support for geojson "crs" and "bbox" members in validation

--- src/geotoolkit/geojson/schema.clj
+++ src/geotoolkit/geojson/schema.clj
@@ -14,6 +14,22 @@
 (def CoordinateList 
   (s/either CoordinateListLevel1 CoordinateListLevel2))
 
+(def BoundingBox 
+  (s/both
+    [s/Num]
+    (s/pred #(= (count %) 4) "has exactly 4 coordinate numbers")))
+
+(def CoordinateReferenceSystem {
+  :type (s/both
+    s/Str
+    (s/pred 
+      (fn [x] (some #(= x %) 
+        ["name" "link"]))
+      "has no valid CoordinateReferenceSystem type"))
+  :properties {
+    (s/either s/Str s/Keyword) s/Any
+    }})
+
 (def Geometry {
   :type (s/both
     s/Str
@@ -22,6 +38,7 @@
         ["Point" "Polygon" "LineString"]))
       "has non-multi geometry type"))
   :coordinates (s/either Coordinate CoordinateList)
+  (s/optional-key :crs) CoordinateReferenceSystem
 })
 
 (def MultiGeometry {
@@ -32,6 +49,7 @@
         ["MultiPoint" "MultiPolygon" "MultiLineString" "GeometryCollection"]))
       "has multigeometry type"))
   :geometries [Geometry]
+  (s/optional-key :crs) CoordinateReferenceSystem
 })
 
 (def Feature { 
@@ -41,10 +59,14 @@
   }
   :type (s/both s/Str 
     (s/pred #(= % "Feature") "type of feature is 'Feature'"))
+  (s/optional-key :bbox) BoundingBox
+  (s/optional-key :crs) CoordinateReferenceSystem
 })
 
 (def FeatureCollection {
   :features [Feature]
   :type (s/both s/Str 
     (s/pred #(= % "FeatureCollection") "type of featurecollection is 'FeatureCollection'"))
+  (s/optional-key :bbox) BoundingBox
+  (s/optional-key :crs) CoordinateReferenceSystem
 })
