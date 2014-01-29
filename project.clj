55c76cb7e8596779e3d55128d58b1e37c313c204
Nico Mandery <nico.mandery@geops.de>
2014-01-29 15:29:32 +0100

    set version number to 0.1.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject geotoolkit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject geotoolkit "0.1.1"
+  :description "A Clojure library designed to work with PostGIS, GeoJSON and related stuff."
+  :url "https://github.com/nmandery/geotoolkit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
